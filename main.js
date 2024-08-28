d06891cb38762068f146ca6acce3aa7be9f8dd1c
dfran6 <awulorfrancis@gmail.com>
2024-08-28 22:40:31 +0100

    added night mode

--- main.js
+++ main.js
@@ -94,6 +94,40 @@ function switchToDay() {
 function switchToNight() {
     dayAmbientLight.intensity = 0;
     nightAmbientLight.intensity = 0.2;
+    
+ //Night Sky Shader
+const skyGeo = new THREE.SphereGeometry(500, 32, 32);
+const skyMat = new THREE.ShaderMaterial({
+  uniforms: {
+    topColor: { value: new THREE.Color(0x030353) },
+    bottomColor: { value: new THREE.Color(0x02022e) },
+    offset: { value: 33 },
+    exponent: { value: 0.6 }
+  },
+  vertexShader: `
+    varying vec3 vWorldPosition;
+    void main() {
+      vec4 worldPosition = modelMatrix * vec4(position, 1.0);
+      vWorldPosition = worldPosition.xyz;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `,
+  fragmentShader: `
+    uniform vec3 topColor;
+    uniform vec3 bottomColor;
+    uniform float offset;
+    uniform float exponent;
+    varying vec3 vWorldPosition;
+    void main() {
+      float h = normalize(vWorldPosition + offset).y;
+      gl_FragColor = vec4(mix(bottomColor, topColor, max(pow(max(h, 0.0), exponent), 0.0)), 1.0);
+    }
+  `,
+  side: THREE.BackSide
+});
+
+const sky = new THREE.Mesh(skyGeo, skyMat);
+scene.add(sky);
 }
 
 // Sky Shader
